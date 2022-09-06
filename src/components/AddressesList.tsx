42ae310e23a4ca8a3668ae700e18dafaeb552782
HJain01 <jainh9999@gmail.com>
2022-09-06 10:48:51 -0400

    added component that displays list of addresses

---
+++ src/components/AddressesList.tsx
@@ -0,0 +1,28 @@
+import {List, ListItem} from "@mui/material";
+import "./AddressesList.css"
+import {useDispatch, useSelector} from "react-redux";
+import {getOrigins, remove} from "../store/slices/originsSlice";
+import DeleteIcon from "@mui/icons-material/Delete";
+
+export default function AddressesList() {
+    let originsList = useSelector(getOrigins);
+    const dispatch = useDispatch();
+
+    const deleteAddress = (index: number) => {
+        dispatch(remove(originsList[index]));
+    }
+
+    return (
+        <List className="list-margin">
+            {originsList.map((address: string, index: number) => {
+                return(
+                    <ListItem className="address-item" secondaryAction={
+                        <DeleteIcon onClick={() => deleteAddress(index)} />
+                    }>
+                        <p className="address">{address}</p>
+                    </ListItem>
+                )
+            })}
+        </List>
+    )
+}
